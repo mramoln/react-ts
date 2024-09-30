e0b470ce76979505288dc4daea32c23a74edd033
Amol Nadagonde <amol.nadagonde@entrata.com>
2024-09-30 08:03:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Basic React App</div>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/, // If using CSS
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/, // If using images
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    hot: true, // Enable Hot Module Replacement
+    open: true, // Open the browser automatically
+  },
+};
