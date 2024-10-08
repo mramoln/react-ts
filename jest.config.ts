b966cb4604ea36bb6c21780322879f41adb34ab0
Amol Nadagonde <amol.nadagonde@entrata.com>
2024-10-08 14:53:14 +0530

    adding test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -6,5 +6,20 @@ export default {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  collectCoverage: true,  // Enable coverage collection
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',   // Specify where to collect coverage from
+    '!src/**/*.d.ts',      // Ignore type definition files
+  ],
+  coverageReporters: ['text', 'lcov'],  // Coverage report formats
+  coverageDirectory: 'coverage',
+  coverageThreshold: { // optional
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
