7a64044d59d247add2fc07b5d8fa773bb508eb27
Amol Nadagonde <amol.nadagonde@entrata.com>
2024-10-11 13:08:56 +0530

    Rectifying the configurations for running the project with TS ESLint and Jests

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { combiner } from './utils/combiner';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   const handleCounterClick = () => {
-    setCount(prevCount => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
   };
-  
-  return <div>Basic React App {count} <button onClick={handleCounterClick}>INC</button></div>;
+
+  useEffect(() => {
+    const combinedText = combiner('Hello', 'World');
+    console.log('combinedText', combinedText);
+    const combinedNumber = combiner(2, 3);
+    console.log('combinedNumber', combinedNumber);
+  }, []);
+
+  return (
+    <div>
+      Basic React App {count} <button onClick={handleCounterClick}>INC</button>
+    </div>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./index.tsx",
+  entry: './index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
     clean: true,
   },
   resolve: {
@@ -14,30 +14,30 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx$/,
+        test: /\.(ts|tsx)?$/,
         exclude: /node_modules/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
         },
       },
       {
         test: /\.css$/, // If using CSS
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/, // If using images
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
